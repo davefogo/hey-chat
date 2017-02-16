8e0af4ae85e25fd4f52d5c485316e4c3eeaae91d
David Forero <davefogo@gmail.com>
2017-02-15 19:34:43 -0500

    refactor

--- dist/scripts/controllers/LandingCtrl.js
+++ dist/scripts/controllers/LandingCtrl.js
@@ -1,28 +1,8 @@
-(function(){
-  function LandingCtrl(Rooms, $uibModal, $rootScope) {
+(function() {
+  function LandingCtrl(Rooms) {
     this.rooms = Rooms.all;
-    //function that pops up the modal
-    this.createRoomModal = function() {
-        $rootScope.uibModalInstance = $uibModal.open({
-        templateUrl: 'templates/modal.html',
-        controller: 'ModalCtrl',
-        size: 'sm',
-        backdrop: true
-      });
-    };
-
-    this.addChatroom = function() {
-      this.rooms.$add({$value: document.getElementById('value').value})
-      $rootScope.uibModalInstance.close('cancel');
-    };
-
-    this.closeNewRoom = function() {
-      $rootScope.uibModalInstance.close('cancel');
-    }
   }
-
-
   angular
     .module('heyChat')
-    .controller('LandingCtrl', ['Rooms', '$uibModal', '$rootScope', LandingCtrl]);
+    .controller('LandingCtrl', ['Rooms', LandingCtrl]);
 })();
--- dist/scripts/controllers/ModalCtrl.js
+++ dist/scripts/controllers/ModalCtrl.js
@@ -1,9 +1,27 @@
-(function(){
-  function ModalCtrl($uibModal){
+(function() {
+  function ModalCtrl(Rooms, $uibModal, $rootScope) {
+    this.rooms = Rooms.all;
+    //function that pops up the modal
+    this.createRoomModal = function() {
+        $rootScope.uibModalInstance = $uibModal.open({
+        templateUrl: 'templates/modal.html',
+        controller: 'ModalCtrl',
+        size: 'sm',
+        backdrop: true
+      });
+    };
 
+    this.addChatroom = function() {
+      this.rooms.$add({$value: document.getElementById('value').value})
+      $rootScope.uibModalInstance.close('cancel');
+    };
+
+    this.closeNewRoom = function() {
+      $rootScope.uibModalInstance.close('cancel');
+    }
   }
 
   angular
     .module('heyChat')
-    .controller('ModalCtrl', ['$uibModal', ModalCtrl]);
+    .controller('ModalCtrl', ['Rooms', '$uibModal', '$rootScope', ModalCtrl]);
 })();
